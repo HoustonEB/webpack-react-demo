588849e3c050f94941a48fcc0f36d6a5030b3b32
v_yuzhuang01 <v_yuzhuang01@baidu.com>
2018-10-17 16:21:14 +0800

    ds

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import {Layout, Menu, Breadcrumb, Icon} from 'antd';
 import CssDemo from './CssDemo';
+import ExcelDemo from './ExcelDemo';
 
 const {SubMenu} = Menu;
 
@@ -16,7 +17,11 @@ const sideRouter = () => (
             <span>Css</span>
             <Link to="/css-demo"></Link>
         </Menu.Item>
-        <Menu.Item key="5">option5</Menu.Item>
+        <Menu.Item key="5">
+            <Icon type="pie-chart" />
+            <span>Excel</span>
+            <Link to="/ExcelDemo"></Link>
+        </Menu.Item>
         <Menu.Item key="9">option9</Menu.Item>
     </Menu>
 );
