523915664cadb524e0964886e08de52a3d85a53a
HoustonEB <860852345@qq.com>
2018-12-11 17:23:41 +0800

    modify webpack-config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,23 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = merge(common, {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist/dev/'),
+        filename: 'main[hash].js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist/dev/']),
+        new HtmlWebpackPlugin({
+            title: 'Dev', // 模板中有title就会替代
+            template: 'template/dev-template.html' // 生成的html模板
+        })
+    ],
     mode: 'development', // 开发模式不会压缩代码
     devtool: 'inline-source-map',// 仅用于development
     // watch: false, // 在webpack-dev-server和webpack-dev-middleware中watch default enabled
