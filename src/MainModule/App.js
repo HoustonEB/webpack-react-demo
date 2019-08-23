b61a0d08118b043a31d829473d212564efb65144
v_yuzhuang01 <v_yuzhuang01@baidu.com>
2019-08-23 19:38:37 +0800

    修改路由

--- src/MainModule/App.js
+++ src/MainModule/App.js
@@ -1,89 +1,148 @@
 import React, {Component} from 'react';
-import {HashRouter, BrowserRouter, Route, Redirect, Link} from "react-router-dom";
+import {HashRouter, BrowserRouter, Route, Redirect, Link, withRouter, Router} from "react-router-dom";
 import {Layout, Menu, Breadcrumb, Icon} from 'antd';
-import SideRouter from './router';
+import routeConfig from './router';
 import './style.main.less';
 import CssDemo from "./CssDemo";
+import StructureTreeContrast from "./StructureTreeContrast";
 import CardTree from "./CardTree";
 import Tree from "./Tree";
-import Transfer from "./Transfer";
-import StructureTreeContrast from "./StructureTreeContrast";
+import DragSort from "./DragSort";
+import Canvas from "./Canvas";
+import Draggable from "./Draggable";
+import Collapse from "./Collapse";
 import JsDemo from "./JsDemo";
-import Draggable from './Draggable';
-import RegExr from './RegExr';
-import Canvas from './Canvas';
-import DragSort from './DragSort';
-import Carousel from './Carousel';
-import Collapse from './Collapse';
-import VirtualList from './VirtualList';
+import RegExr from "./RegExr";
+import Transfer from "./Transfer";
+import Carousel from "./Carousel";
+import VirtualList from "./VirtualList";
 
 const Header = Layout.Header;
 const Content = Layout.Content;
 const Sider = Layout.Sider;
-// const routeConfig = [
-//     { path: '/',
-//         component: App,
-//         indexRoute: { component: CssDemo },
-//         childRoutes: [
-//             { path: 'about', component: About },
-//             { path: 'inbox',
-//                 component: Inbox,
-//                 childRoutes: [
-//                     { path: '/messages/:id', component: Message },
-//                     { path: 'messages/:id',
-//                         onEnter: function (nextState, replaceState) {
-//                             replaceState(null, '/messages/' + nextState.params.id)
-//                         }
-//                     }
-//                 ]
-//             }
-//         ]
-//     }
-// ];
 
-const routeConfig = [
-    { path: '/',
-        component: CssDemo,
-        indexRoute: { component: CssDemo },
-        childRoutes: [
-            { path: '/css-demo', component: CssDemo },
-            { path: '/card-tree', component: CardTree },
-            { path: '/tree', component: new Tree().defaultComp },
-            { path: '/transfer', component: new Transfer().defaultComp },
-            { path: '/structure-tree-contrast', component: new StructureTreeContrast().defaultComp },
-            { path: '/js-demo', component: new JsDemo().defaultComp },
-            { path: '/draggable', component: new Draggable().defaultComp },
-            { path: '/reg-exr', component: new RegExr().defaultComp },
-            { path: '/canvas', component: new Canvas().defaultComp},
-            { path: '/drag-sort', component: new DragSort().defaultComp},
-            { path: '/collapse', component: Collapse}
-        ]
-    }
+const MenuLink = [
+    {
+        key: '/css-demo',
+        url: '/css-demo',
+        sideBarName: 'Css'
+    },
+    {
+        key: '/card-tree',
+        url: '/card-tree',
+        sideBarName: 'CardTree'
+    },
+    {
+        key: '/tree',
+        url: '/tree',
+        sideBarName: 'Tree'
+    },
+    {
+        key: '/transfer',
+        url: '/transfer',
+        sideBarName: '穿梭框'
+    },
+    {
+        key: '/structure-tree-contrast',
+        url: '/structure-tree-contrast',
+        sideBarName: '结构树对比'
+    },
+    {
+        key: '/js-demo',
+        url: '/js-demo',
+        sideBarName: 'JsDemo'
+    },
+    {
+        key: '/draggable',
+        url: '/draggable',
+        sideBarName: 'draggable'
+    },
+    {
+        key: '/reg-exr',
+        url: '/reg-exr',
+        sideBarName: 'RegExr'
+    },
+    {
+        key: '/canvas',
+        url: '/canvas',
+        sideBarName: 'Canvas'
+    },
+    {
+        key: '/drag-sort',
+        url: '/drag-sort',
+        sideBarName: '拖拽排序'
+    },
+    {
+        key: '/collapse',
+        url: '/collapse',
+        sideBarName: '折叠面板'
+    },
+    {
+        key: '/carousel',
+        url: '/carousel',
+        sideBarName: '轮播图'
+    },
+    {
+        key: '/virtual-list',
+        url: '/virtual-list',
+        sideBarName: '虚拟列表'
+    },
 ];
+
+const SideRouter = withRouter(
+    (history) => {
+        let link = MenuLink.map(item => {
+            const {url, key, sideBarName} = item;
+            return (
+                <Menu.Item key={key} style={{width: '100%'}}>
+                    <span>{sideBarName}</span>
+                    <Link to={url}></Link>
+                </Menu.Item>
+            )
+        });
+        return (
+            <Menu
+                mode="inline"
+                style={{borderRight: 'none'}}
+                defaultSelectedKeys={['/css-demo']}
+                selectedKeys={[history.location.pathname]}
+            >{link}</Menu>
+        )
+    }
+);
+
+const RouteWithSubRoutes = (route) => {
+    return (
+        <Route
+            path={route.path}
+            render={props => (
+                // pass the sub-routes down to keep nesting
+                <route.component {...props} routes={route.routes} />
+            )}
+        />
+    );
+    // return (
+    //     <Route
+    //         path={route.path}
+    //         render={props => (
+    //             // pass the sub-routes down to keep nesting
+    //             <route.component {...props} routes={route.routes} />
+    //         )}
+    //     />
+    // );
+};
 
 export default class App extends Component {
 
-    renderHeader() {
-        return (
-            <Header className="header">
-                <div className="logo"/>
-                <Menu
-                    theme="dark"
-                    mode="horizontal"
-                    defaultSelectedKeys={['1']}
-                    style={{lineHeight: '64px'}}
-                >
-                    <Menu.Item key="1">nav 1</Menu.Item>
-                    <Menu.Item key="2">nav 2</Menu.Item>
-                    <Menu.Item key="3">nav 3</Menu.Item>
-                </Menu>
-            </Header>
-        )
-    }
-
     renderSider() {
         return (
-            <Sider width={200} style={{background: '#fff'}}>
+            <Sider style={{
+                overflow: 'auto',
+                height: '100vh',
+                position: 'fixed',
+                left: 0,
+                backgroundColor: '#fff',
+                borderRight: '1px solid #eee'}}>
                 <SideRouter />
             </Sider>
         )
@@ -91,22 +150,23 @@ export default class App extends Component {
 
     renderContent() {
         return (
-            <Layout style={{padding: '24px', height: '90vh'}}>
-                <Content style={{background: '#fff', padding: 24, margin: 0, minHeight: 350}}>
-                    <Route exact path="/" render={() => (<Redirect to="/css-demo"/>)}/> {/*首次进入跳转到指定由*/}
-                    <Route exact path="/css-demo" component={CssDemo}/>
-                    <Route exact path="/card-tree" component={CardTree}/>
-                    <Route exact path="/tree" component={new Tree().defaultComp}/>
-                    <Route exact path="/transfer" component={new Transfer().defaultComp}/>
-                    <Route exact path="/structure-tree-contrast" component={new StructureTreeContrast().defaultComp}/>
-                    <Route exact path="/js-demo" component={new JsDemo().defaultComp}/>
-                    <Route exact path="/draggable" component={new Draggable().defaultComp}/>
-                    <Route exact path="/reg-exr" component={new RegExr().defaultComp}/>
-                    <Route exact path="/canvas" component={new Canvas().defaultComp}/>
-                    <Route exact path="/drag-sort" component={new DragSort().defaultComp}/>
-                    <Route exact path="/carousel" component={Carousel}/>
-                    <Route exact path="/collapse" component={Collapse}/>
-                    <Route exact path="/virtual-list" component={VirtualList}/>
+            <Layout style={{height: '100vh'}}>
+                <Content style={{background: '#fff', padding: 24, marginLeft: 200, minHeight: 350}}>
+                    {/*{routeConfig.map((route, i) => <RouteWithSubRoutes key={i} {...route} />)}*/}
+                    {/*<Route exact path="/" render={() => (<Redirect to="/css-demo"/>)}/> /!*首次进入跳转到指定由*!/*/}
+                    {/*<Route exact path="/css-demo" component={CssDemo}/>*/}
+                    {/*<Route exact path="/card-tree" component={CardTree}/>*/}
+                    {/*<Route exact path="/tree" component={new Tree().defaultComp}/>*/}
+                    {/*<Route exact path="/transfer" component={new Transfer().defaultComp}/>*/}
+                    {/*<Route exact path="/structure-tree-contrast" component={new StructureTreeContrast().defaultComp}/>*/}
+                    {/*<Route exact path="/js-demo" component={new JsDemo().defaultComp}/>*/}
+                    {/*<Route exact path="/draggable" component={new Draggable().defaultComp}/>*/}
+                    {/*<Route exact path="/reg-exr" component={new RegExr().defaultComp}/>*/}
+                    {/*<Route exact path="/canvas" component={new Canvas().defaultComp}/>*/}
+                    {/*<Route exact path="/drag-sort" component={new DragSort().defaultComp}/>*/}
+                    {/*<Route exact path="/carousel" component={Carousel}/>*/}
+                    {/*<Route exact path="/collapse" component={Collapse}/>*/}
+                    {/*<Route exact path="/virtual-list" component={VirtualList}/>*/}
                 </Content>
             </Layout>
         )
@@ -115,9 +175,8 @@ export default class App extends Component {
     renderContainer() {
         return (
             <Layout>
-                {/*{this.renderHeader()}*/}
-                    {this.renderSider()}
-                    {this.renderContent()}
+                {this.renderSider()}
+                {this.renderContent()}
             </Layout>
         )
     }
@@ -125,12 +184,10 @@ export default class App extends Component {
     render() {
         console.log(process.env.NODE_ENV, '当前环境')
         return (
-            <div>
+            <HashRouter>
                 {/*react-router 4.0.0 version router删除用hashRouter替代 => 解决原地刷新页面报错(get不到页面)*/}
-                <HashRouter routes={routeConfig}>
-                    {this.renderContainer()}
-                </HashRouter>
-            </div>
+                {this.renderContainer()}
+            </HashRouter>
         )
     }
 }
--- src/MainModule/router.js
+++ src/MainModule/router.js
@@ -1,94 +1,34 @@
-import React from 'react';
-import {Route, Link, withRouter} from "react-router-dom";
-import {Menu, Icon} from 'antd';
+import CssDemo from "./CssDemo";
+import StructureTreeContrast from "./StructureTreeContrast";
+import CardTree from "./CardTree";
+import Tree from "./Tree";
+import DragSort from "./DragSort";
+import Canvas from "./Canvas";
+import Draggable from "./Draggable";
+import Collapse from "./Collapse";
+import JsDemo from "./JsDemo";
+import RegExr from "./RegExr";
+import Transfer from "./Transfer";
 
-const MenuLink = [
+const routeConfig = [
     {
-        key: '/css-demo',
-        url: '/css-demo',
-        sideBarName: 'Css'
-    },
-    {
-        key: '/card-tree',
-        url: '/card-tree',
-        sideBarName: 'CardTree'
-    },
-    {
-        key: '/tree',
-        url: '/tree',
-        sideBarName: 'Tree'
-    },
-    {
-        key: '/transfer',
-        url: '/transfer',
-        sideBarName: '穿梭框'
-    },
-    {
-        key: '/structure-tree-contrast',
-        url: '/structure-tree-contrast',
-        sideBarName: '结构树对比'
-    },
-    {
-        key: '/js-demo',
-        url: '/js-demo',
-        sideBarName: 'JsDemo'
-    },
-    {
-        key: '/draggable',
-        url: '/draggable',
-        sideBarName: 'draggable'
-    },
-    {
-        key: '/reg-exr',
-        url: '/reg-exr',
-        sideBarName: 'RegExr'
-    },
-    {
-        key: '/canvas',
-        url: '/canvas',
-        sideBarName: 'Canvas'
-    },
-    {
-        key: '/drag-sort',
-        url: '/drag-sort',
-        sideBarName: '拖拽排序'
-    },
-    {
-        key: '/collapse',
-        url: '/collapse',
-        sideBarName: '折叠面板'
-    },
-    {
-        key: '/carousel',
-        url: '/carousel',
-        sideBarName: '轮播图'
-    },
-    {
-        key: '/virtual-list',
-        url: '/virtual-list',
-        sideBarName: '虚拟列表'
-    },
-];
-const SideRouter = withRouter(
-    (history) => {
-        let link = MenuLink.map(item => {
-            const {url, key, sideBarName} = item;
-            return (
-                <Menu.Item key={key}>
-                    <span>{sideBarName}</span>
-                    <Link to={url}></Link>
-                </Menu.Item>
-        )
-        });
-        return (
-            <Menu
-                mode="inline"
-                style={{height: '100%', borderRight: 0}}
-                // defaultSelectedKeys={['/css-demo']}
-                selectedKeys={[history.location.pathname]}
-            >{link}</Menu>
-        )
+        path: '/',
+        component: CssDemo,
+        indexRoute: { component: CssDemo },
+        routes: [
+            { path: '/css-demo', component: CssDemo },
+            { path: '/card-tree', component: CardTree },
+            { path: '/tree', component: new Tree().defaultComp },
+            { path: '/transfer', component: new Transfer().defaultComp },
+            { path: '/structure-tree-contrast', component: new StructureTreeContrast().defaultComp },
+            { path: '/js-demo', component: new JsDemo().defaultComp },
+            { path: '/draggable', component: new Draggable().defaultComp },
+            { path: '/reg-exr', component: new RegExr().defaultComp },
+            { path: '/canvas', component: new Canvas().defaultComp},
+            { path: '/drag-sort', component: new DragSort().defaultComp},
+            { path: '/collapse', component: Collapse}
+        ]
     }
-);
+];
 
-export default SideRouter;
+export default routeConfig;
