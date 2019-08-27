c1bd5bbca5a121992962c2162ef5414c782da5d1
HoustonEB <860852345@qq.com>
2019-08-27 17:26:22 +0800

    change travis.yml

--- scripts/push.sh
+++ scripts/push.sh
@@ -6,6 +6,6 @@ git config user.name "${U_NAME}"
 git config user.email "${U_EMAIL}"
 git add .
 git commit -m "deploy"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF} master:gh-pages"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
 echo ------push gh-pages success--------
 cd -
