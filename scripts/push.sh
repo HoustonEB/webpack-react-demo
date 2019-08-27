eac5e520eb60e52918bc0d66a9d3914e6769f050
HoustonEB <860852345@qq.com>
2019-08-27 17:16:25 +0800

    add travis token

--- scripts/push.sh
+++ scripts/push.sh
@@ -2,10 +2,10 @@ set -e
 echo ------------push gh-pages start----------------------
 cd ./dist/prod/
 git init
-git config user.name HoustonEB
-git config user.email 860852345@qq.com
+git config user.name "${U_NAME}"
+git config user.email "${U_EMAIL}"
 git add .
-git commit -m 'deploy'
-git push --force https://github.com/HoustonEB/webpack-react-demo.git master:gh-pages
+git commit -m "deploy"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}master:gh-pages"
 echo ------push gh-pages success--------
 cd -
