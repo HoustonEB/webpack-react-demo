07337f1ca4c8ec76161c51d93b30a3c3c2718a90
HoustonEB <860852345@qq.com>
2019-11-20 11:25:55 +0800

    添加锚点组件

--- plop/plop-templates/single/index.js
+++ plop/plop-templates/single/index.js
@@ -2,9 +2,14 @@ import React, {Component} from 'react';
 import './style.use.less';
 
 export default class {{compName}} extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
-            <div className={'{{compName}}-wrapper'}></div>
+            <div className={'{{compName}}-wrapper'}>{{compName}}</div>
         )
     }
 }
