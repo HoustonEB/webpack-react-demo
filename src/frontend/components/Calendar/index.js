2395168310124136b4a4a812834b47a4e84933f6
HoustonEB <860852345@qq.com>
2020-08-14 15:55:21 +0800

    docs:文档,日历开发

--- src/frontend/components/Calendar/index.js
+++ src/frontend/components/Calendar/index.js
@@ -1,9 +1,11 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { h, c, isCallable, noop } from '../utils';
 import moment from 'moment';
+import {ui} from '../locale';
 import PropTypes from 'prop-types';
-import c from 'classnames';
 import './style.use.less';
-
+console.log(ui, 'ui')
+const DATE_ROW = 6;
 const DATE_COL = 7;
 export default class Calendar extends Component {
     static propTypes = {
@@ -33,22 +35,21 @@ export default class Calendar extends Component {
 
     renderDateHeader() {
         let localData = this.localeData;
-        let firstDayOfWeek = localData.firstDayOfWeek();
+        let firstDayOfWeek = localData.firstDayOfWeek(); // 一周的第一天是星期几,英文是星期日,中文是周一.
         let weekDays = [];
         const now = moment();
         for (let index = 0; index < DATE_COL; index++) {
             let dayIndex = (firstDayOfWeek + index) % DATE_COL;
-            now.day(dayIndex);
-            weekDays[index] = localData.weekdaysMin(now);
+            now.day(dayIndex); // 设置时间是周几
+            weekDays[index] = localData.weekdaysMin(now); // 获取传入时间是周几
+            // 多余的处理
         }
-        console.log(weekDays, 'now')
         const headerClass = this.dateClass + '-header';
         const headerRowClass = `${headerClass}-row`;
         const headerCellClass = `${headerClass}-cell`;
         return (
-            <thead className={headerClass}>
-            <tr className={headerRowClass}>
-                {
+            h.thead(headerClass, {},
+                h.tr(headerRowClass, {},
                     weekDays.map((day, idx) => {
                         return (
                             <th
@@ -58,9 +59,8 @@ export default class Calendar extends Component {
                             </th>
                         )
                     })
-                }
-            </tr>
-            </thead>
+                )
+            )
         )
     }
 
@@ -74,13 +74,15 @@ export default class Calendar extends Component {
             className,
         } = this.props;
         return (
-            <div className={c(classPrefix,'-calendar-wrapper', className)}>
-                {this.renderHeader()}
-                <table>
-                    {this.renderDateHeader()}
-                    {this.renderDateBody()}
-                </table>
-            </div>
+            h.div('', {
+                className: c(classPrefix, '-calendar-wrapper', className)
+            },
+                this.renderHeader(),
+                h.table('', {},
+                    this.renderDateHeader(),
+                    this.renderDateBody()
+                )
+            )
         )
     }
 
@@ -95,4 +97,9 @@ export default class Calendar extends Component {
     get compClass() {
         return this.props.classPrefix + '-calendar';
     }
+}
+
+function getThisMoment() {
+    const today = moment();
+    return today.locale();
 }
